0ef690076f0db678bb9deba180c15a47c9c6e0d7
dennis nyaga <murildennis7@gmail.com>
2024-09-26 00:38:41 +0300

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>page customers</div>
+  )
+}
+
+export default page
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>page invoice</div>
+  )
+}
+
+export default page
