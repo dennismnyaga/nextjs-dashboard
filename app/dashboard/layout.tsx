acddfc602ea6cd494740a2ac5892c3a2c1e5dfd9
dennis nyaga <murildennis7@gmail.com>
2024-09-28 18:26:15 +0300

    finished the tutorial

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,16 @@
-import SideNav from '@/app/ui/dashboard/sidenav';
+import { Metadata } from 'next';
  
+
+
+import SideNav from '@/app/ui/dashboard/sidenav';
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
+
+
+export const experimental_ppr = true;
+
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
